4e37d402562b0a743ef6b5d3f90e79683fc836fd
PC <PC@>
2021-11-14 01:37:24 +0300

    задача 3 проверка на 4 числа проверка на точки

--- module11/task3/task3.cpp
+++ module11/task3/task3.cpp
@@ -1,6 +1,70 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+bool corrcetSymbol(std::string ip) {
+  int pointCount = 0;
+  for (int i = 0; i < ip.length(); i++) {
+    if (ip[i] == '.') ++pointCount;
+
+    if (ip[i] == '.' || (ip[i] >= '0' && ip[i] <= '9')) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  if (pointCount != 3) return false;
+  return true;
+}
+
+bool corrcetIp(std::string ip) {
+  if (corrcetSymbol(ip)) {
+    std::string num1 = ip.substr(0, ip.find('.'));
+    if (num1.empty() || ip.empty()) {
+      return false;
+    } else {
+      ip.erase(0, ip.find('.') + 1);
+    }
+
+    std::string num2 = ip.substr(0, ip.find('.'));
+    if (num2.empty() || ip.empty()) {
+      return false;
+    } else {
+      ip.erase(0, ip.find('.') + 1);
+    }
+
+    std::string num3 = ip.substr(0, ip.find('.'));
+    if (num3.empty() || ip.empty()) {
+      return false;
+    } else {
+      ip.erase(0, ip.find('.') + 1);
+    }
+
+    if (ip.empty() || ip.find('.') != std::string::npos){
+      return false;
+    } else {
+      std::string num4 = ip;
+    }
+
+    return true;
+  } else {
+    return false;
+  }
+        
+
+	//for (int i = 0; i < ip.length(); i++) {
+	//		
+	//}
+
+
+}
+
+int main() {
+	std::string ipAddress;
+	std::cout << "Enter IP address:" << std::endl;
+	std::cin >> ipAddress;
+
+  if (corrcetIp(ipAddress)) {
+    std::cout << "IP address is CORRECT";
+  } else {
+    std::cout << "INVALID IP ADDRESS";
+  }
 }

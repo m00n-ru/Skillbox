e23aec661be2031aad16a062166aad4a0017f262
PC <PC@>
2021-11-18 21:07:02 +0300

    поправлены отрицательные значения Добавлено отслеживание нуля

--- module11/task4/task4.cpp
+++ module11/task4/task4.cpp
@@ -22,16 +22,18 @@ int compareFunc(std::string num1, std::string num2, int parI, int parJ) {
   int i;
   int j;
   bool point = false;
-  for (i = parI; i < num1.length() && num1[i] == '0' && num1.length() != 1;
-       i++) {
+  for (i = parI;
+       i < num1.length() && num1[i] == '0' && num1.length() != 1 + parI; i++) {
   }
-  for (j = parJ; j < num2.length() && num2[j] == '0' && num2.length() != 1;
-       j++) {
+  for (j = parJ;
+       j < num2.length() && num2[j] == '0' && num2.length() != 1 + parJ; j++) {
   }
   bool endStr1 = false;
   bool endStr2 = false;
+  bool zeroCheck = true;
   int length = std::max(num1.length() - i, num2.length() - j);
   for (int k = 0; k < length; k++) {
+    if (num1[i] >= '1' && num1[i] <= '9') zeroCheck = false;
     if (num1[i] == '.') point = true;
     if (num2[j] == '.') point = true;
     bool check = k == 0 && point && (num1[i] == '0' || num2[j] == '0');
@@ -65,15 +67,24 @@ int compareFunc(std::string num1, std::string num2, int parI, int parJ) {
     (i < num1.length() - 1) ? i++ : endStr1 = true;
     (j < num2.length() - 1) ? j++ : endStr2 = true;
   }
+  if (zeroCheck && parI != parJ) return 3;
   return 0;
 }
 
 int compareNum(std::string num1, std::string num2) {
   if (correctNum(num1) && correctNum(num2)) {
     if (num1[0] != '-' && num2[0] == '-') {
-      return compareFunc(num1, num2, 0, 1);
+      if (compareFunc(num1, num2, 0, 1) == 3) {
+        return 0;
+      } else {
+        return 1;
+      }
     } else if (num1[0] == '-' && num2[0] != '-') {
-      return compareFunc(num1, num2, 1, 0);
+      if (compareFunc(num1, num2, 1, 0) == 3) {
+        return 0;
+      } else {
+        return -1;
+      }
     } else if (num1[0] != '-' && num2[0] != '-') {
       return compareFunc(num1, num2, 0, 0);
     } else {

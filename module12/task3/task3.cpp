88638e482395e8dc6641c670758bc3b1634b4617
PC <PC@>
2021-11-20 14:54:38 +0300

    задача 3

---
+++ module12/task3/task3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main() {
+  float numbers[15];
+  std::cout << "Enter numbers:" << std::endl;
+  for (int i = 0; i < 15; i++) {
+    std::cin >> numbers[i];
+  }
+
+  std::cout << "Output: " << std::endl;
+
+  for (int i = 0; i < 15; i++) {
+    for (int j = 0; j < 15; j++) {
+      if (numbers[i] < numbers[j]) {
+        float buff = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = buff;
+      }
+
+      if (i == 14) std::cout << numbers[j] << " ";
+    }
+  }
+}

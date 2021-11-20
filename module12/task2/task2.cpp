2e966255368e81191329d085b7a8ddae31b29f9c
PC <PC@>
2021-11-20 16:02:42 +0300

    2 задача через 1 проход

--- module12/task2/task2.cpp
+++ module12/task2/task2.cpp
@@ -3,18 +3,13 @@
 int main() {
   int numbers[15] = {114, 111, 106, 107, 108, 105, 115, 108,
                      110, 109, 112, 113, 116, 117, 118};
-  int repitNum = 0;
+  int sum = 0;
+  int min = numbers[0];
   for (int i = 0; i < 15; i++) {
-    for (int j = 0; j < 15; j++) {
-      if (i == j) {
-        continue;
-      } else {
-        if (numbers[i] == numbers[j]) {
-          repitNum = numbers[j];
-          break;
-        }
-      }
-    }
+    sum += numbers[i] - i;
+    if (numbers[i] <= min) min = numbers[i];
   }
+  int repitNum = sum + 14 - 14 * min;
+
   std::cout << "Repeating number: " << repitNum;
 }

2e231ddc18f391d1b8c51d80dbe97a0083ae3793
PC <PC@>
2021-11-20 13:27:38 +0300

    module 12  задача 1

---
+++ module12/task1/task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main() {
+  int apartNum;
+  std::string name[10];
+  std::cout << "Enter 10 names of tenants: " << std::endl;
+  for (int i = 0; i < 10; i++) {
+    std ::cin >> name[i];
+  }
+  std::cout << "Enter apartment number;" << std ::endl;
+  for (int i = 0; i < 3; i++) {
+    std::cin >> apartNum;
+    if (apartNum < 1 || apartNum > 10) {
+      std::cout << "ERROR! Incorrect apartment number" << std::endl;
+    } else {
+      std::cout << name[apartNum - 1] << " lives in aprtment " << apartNum
+                << std::endl;
+    }
+  }
+}

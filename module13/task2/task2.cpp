5d37c9504dbab24c32a539c2fa4c7b1a1d36c931
PC <PC@>
2021-11-21 12:12:02 +0300

    задача 2

---
+++ module13/task2/task2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<float> price = {2.5, 4.25, 3.0, 10.0};
+  std::vector<int> buy = {1, 1, 0, 3};
+  float sumBuy = 0;
+  bool error = false;
+  for (int i = 0; i < buy.size(); i++) {
+    if (buy[i] >= 0 || buy[i] < price.size()) {
+      if (price[buy[i]] > 0) {
+        sumBuy += price[buy[i]];
+      } else {
+        std::cout << "ERROR. In position " << buy[i] << " price vector"
+                  << std::endl;
+        error = true;
+      }
+    } else {
+      std::cout << "ERROR. In position " << i << " buy vector" << std::endl;
+      error = true;
+    }
+  }
+  if (!error) std::cout << "Total cost: " << sumBuy;
+}

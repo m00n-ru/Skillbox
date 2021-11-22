ee0650c27d65372da9b46d27946917a0f2ecdcb0
PC <PC@>
2021-11-22 22:31:21 +0300

    задача 3

---
+++ module14/task3/task3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+  int snake[5][5];
+  for (int i = 0; i < 25; i++) {
+    int a = (i / 5) % 2;
+    snake[i / 5][i % 5] = (i + (9 + 5 * (i / 5 - 1)) * a) - (i + i % 5) * a;
+  }
+
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      std::cout << snake[i][j] << "\t";
+    }
+    std::cout << std::endl;
+  }
+}

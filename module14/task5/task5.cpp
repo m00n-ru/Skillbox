8a1dd5c672c1625c5c3f25d1a7aa7fa1f60d88cc
PC <PC@>
2021-11-23 19:28:56 +0300

    задача5

---
+++ module14/task5/task5.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+void input(float a[][4]) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      std::cin >> a[i][j];
+    }
+  }
+}
+
+void output(float a[][4]) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      std::cout << a[i][j];
+    }
+    std::cout << std::endl;
+  }
+}
+
+int main() {
+  float a[4][4];
+	std::cout << "Input: ";
+  input(a);
+
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (i != j) a[i][j] = 0;
+    }
+  }
+
+  output(a);
+}

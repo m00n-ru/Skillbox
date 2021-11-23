3e905626cd4d48b002ed17ca5178a6ca7dcf1362
PC <PC@>
2021-11-23 19:27:24 +0300

    задача4

---
+++ module14/task4/task4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+void input(int a[][4]) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      std::cin >> a[i][j];
+    }
+  }
+}
+
+int main() {
+  int a[4][4];
+  int b[4][4];
+
+  std::cout << "Input 1: ";
+  input(a);
+  std::cout << "Input 2: ";
+  input(b);
+
+  bool equal = true;
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (a[i][j] != b[i][j]) {
+        equal = false;
+        break;
+      }
+    }
+  }
+  if (equal) {
+    std::cout << "equal";
+  } else {
+    std::cout << "unequal";
+  }
+}

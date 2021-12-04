7839db10e3367b2734a413839cab14d1b8e879f1
PC <PC@>
2021-12-04 11:20:10 +0300

    задача 3

---
+++ module16/task3/task3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <sstream>
+
+int main() {
+  std::string str;
+  std::cout << "Enter the expression: ";
+  std::cin >> str;
+
+  std::stringstream expr{str};
+
+  double a;
+  double b;
+  char c;
+
+  expr >> a >> c >> b;
+
+  if (c == '+') {
+    std::cout << a << "+" << b << "= " << a + b;
+  } else if (c == '-') {
+    std::cout << a << "-" << b << "= " << a - b;
+  } else if (c == '*') {
+    std::cout << a << "*" << b << "= " << a * b;
+  } else if (c == '/') {
+    std::cout << a << "/" << b << "= " << a / b;
+  } else {
+    std::cout << "Error!";
+  }
+}

e30bc69100f3e71c847090e3887b0a5614d925b0
PC <PC@>
2021-12-05 00:46:38 +0300

    задача 1

---
+++ module 17/task1/task1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+void swap(int* a, int* b) {
+  int buff;
+  buff = *a;
+  *a = *b;
+  *b = buff;
+}
+
+int main() {
+  int a = 10;
+  int b = 20;
+  swap(&a, &b);
+  std::cout << a << " " << b;
+}

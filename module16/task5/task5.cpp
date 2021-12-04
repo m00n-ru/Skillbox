62166fa2232688d16c32138bb26a2c1cc043f5f4
PC <PC@>
2021-12-04 13:46:49 +0300

    задача 5 вывод статусов v1

---
+++ module16/task5/task5.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <sstream>
+#include <bitset>
+
+enum mang : const int {
+  mangAllHome = 0,
+  mangElectHome,
+  mangLigthIn,
+  mangLigthOut,
+  mangHeatingHome,
+  mangHeatingWater,
+  mangCondi
+};
+
+std::string statusText(std::bitset<8> status) {
+	std::string text;
+	if (!status.test(mangAllHome)) {
+		text += "Power OFF";
+	} else {
+		text += "Power ON \n";
+		if (status.test(mangElectHome)) {
+			text += "Electrical outlets: ON\n";
+		} else {
+			text += "Electrical outlets: OFF\n";
+		}
+		if (status.test(mangLigthIn)) {
+			text += "Light inside: ON\n";
+		} else {
+			text += "Light inside: OFF\n";
+		}
+		if (status.test(mangLigthOut)) {
+			text += "Light outside: ON\n";
+		} else {
+			text += "Light outside: OFF\n";
+		}
+		if (status.test(mangHeatingHome)) {
+			text += "Heating home: ON\n";
+		} else {
+			text += "Heating home: OFF\n";
+		}
+		if (status.test(mangHeatingWater)) {
+			text += "Heating waterpipe: ON\n";
+		} else {
+			text += "Heating waterpipe: OFF\n";
+		}
+		if (status.test(mangCondi)) {
+			text += "conditioner: On\n";
+		} else {
+			text += "conditioner: OFF\n";
+		}
+	}
+	return text;
+}
+
+
+int main() {
+  //double sensTempOut;
+  //double sensTempIn;
+  //std::string sensMotion;
+  //std::string sensLigth;
+
+	std::bitset<8> status{0b00101011};
+
+
+  //std::cout << "Enter sensor (Temp IN, Temp OUT, Motion, Ligth): ";
+  //std::string input;
+  //std::stringstream sens;
+  //getline(std::cin, input);
+  //sens << input;
+  //sens >> sensTempOut >> sensTempIn >> sensMotion >> sensLigth;
+
+	std::cout << statusText(status);
+
+  return 0;
+}

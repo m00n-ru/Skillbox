ed05d59a7fede7e915043c1a983e7cbacef0437e
PC <PC@>
2021-12-05 00:55:29 +0300

    задача 2

---
+++ module 17/task2/task2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+void revers(int* a) {
+  for (int i = 0; i < 5; i++) {
+    int buff;
+    buff = *(a + i);
+    *(a + i) = *(a + 9 - i);
+    *(a + 9 - i) = buff;
+  }
+}
+
+int main() {
+  int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  revers(a);
+  for (int i = 0; i < 10; i++) {
+    std::cout << *(a + i);
+  }
+}

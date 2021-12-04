9b7c8a82d4f951d87d4f8db9b1aa6bb2bf669716
PC <PC@>
2021-12-05 01:16:06 +0300

    задача 3

---
+++ module 17/task3/task3.cpp
@@ -0,0 +1,14 @@
+﻿#include <cstring>
+#include <iostream>
+
+bool substr(const char* a, const char* b) {
+  if (std::strstr(a, b) == nullptr) return false;
+  return true;
+}
+
+int main() {
+  const char* a = "Hello world";
+  const char* b = "wor";
+  const char* c = "banana";
+  std::cout << substr(a, b) << " " << substr(a, c);
+}

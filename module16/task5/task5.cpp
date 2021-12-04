ff828c48e13e5ac08c16632f13a9fef624089720
PC <PC@>
2021-12-04 14:23:21 +0300

    вывод статусов v2 вывод при изменение температура света

--- module16/task5/task5.cpp
+++ module16/task5/task5.cpp
@@ -1,9 +1,9 @@
-﻿#include <iostream>
+﻿#include <bitset>
+#include <iostream>
 #include <sstream>
-#include <bitset>
 
 enum mang : const int {
-  mangAllHome = 0,
+  mangAllHome,
   mangElectHome,
   mangLigthIn,
   mangLigthOut,
@@ -12,64 +12,81 @@ enum mang : const int {
   mangCondi
 };
 
-std::string statusText(std::bitset<8> status) {
-	std::string text;
-	if (!status.test(mangAllHome)) {
-		text += "Power OFF";
-	} else {
-		text += "Power ON \n";
-		if (status.test(mangElectHome)) {
-			text += "Electrical outlets: ON\n";
-		} else {
-			text += "Electrical outlets: OFF\n";
-		}
-		if (status.test(mangLigthIn)) {
-			text += "Light inside: ON\n";
-		} else {
-			text += "Light inside: OFF\n";
-		}
-		if (status.test(mangLigthOut)) {
-			text += "Light outside: ON\n";
-		} else {
-			text += "Light outside: OFF\n";
-		}
-		if (status.test(mangHeatingHome)) {
-			text += "Heating home: ON\n";
-		} else {
-			text += "Heating home: OFF\n";
-		}
-		if (status.test(mangHeatingWater)) {
-			text += "Heating waterpipe: ON\n";
-		} else {
-			text += "Heating waterpipe: OFF\n";
-		}
-		if (status.test(mangCondi)) {
-			text += "conditioner: On\n";
-		} else {
-			text += "conditioner: OFF\n";
-		}
-	}
-	return text;
+std::string statusText(std::bitset<7> statusOld, std::bitset<7> statusNew,
+                       int hour) {
+  std::string text;
+  if (statusOld.test(mangAllHome) != statusNew.test(mangAllHome))
+    if (!statusNew.test(mangAllHome)) {
+      text += "Power OFF\n";
+      statusNew = 0;
+    } else {
+      text += "Power ON\n";
+    }
+
+  if (statusOld.test(mangElectHome) != statusNew.test(mangElectHome))
+    if (statusNew.test(mangElectHome)) {
+      text += "Electrical outlets: ON\n";
+    } else {
+      text += "Electrical outlets: OFF\n";
+    }
+  if (statusOld.test(mangLigthIn) != statusNew.test(mangLigthIn) ||
+      (hour >= 16 && hour <= 24))
+    if (statusNew.test(mangLigthIn)) {
+      text += "Light inside: ON";
+      int lightTemp = 5000;
+      if (hour >= 16 && hour < 20) {
+        lightTemp -= 460 * (hour - 15);
+      } else if (hour >= 20 && hour <= 23) {
+        lightTemp = 2700;
+      }
+      text += " (LightTemperature = " + std::to_string(lightTemp) + ")\n";
+    } else {
+      text += "Light inside: OFF\n";
+    }
+  if (statusOld.test(mangLigthOut) != statusNew.test(mangLigthOut))
+    if (statusNew.test(mangLigthOut)) {
+      text += "Light outside: ON\n";
+    } else {
+      text += "Light outside: OFF\n";
+    }
+  if (statusOld.test(mangHeatingHome) != statusNew.test(mangHeatingHome))
+    if (statusNew.test(mangHeatingHome)) {
+      text += "Heating home: ON\n";
+    } else {
+      text += "Heating home: OFF\n";
+    }
+  if (statusOld.test(mangHeatingWater) != statusNew.test(mangHeatingWater))
+    if (statusNew.test(mangHeatingWater)) {
+      text += "Heating waterpipe: ON\n";
+    } else {
+      text += "Heating waterpipe: OFF\n";
+    }
+  if (statusOld.test(mangCondi) != statusNew.test(mangCondi))
+    if (statusNew.test(mangCondi)) {
+      text += "conditioner: On\n";
+    } else {
+      text += "conditioner: OFF\n";
+    }
+  return text;
 }
 
-
 int main() {
-  //double sensTempOut;
-  //double sensTempIn;
-  //std::string sensMotion;
-  //std::string sensLigth;
+  // double sensTempOut;
+  // double sensTempIn;
+  // std::string sensMotion;
+  // std::string sensLigth;
 
-	std::bitset<8> status{0b00101011};
+  std::bitset<7> statusOld{0b0101111};
+  std::bitset<7> statusNew{0b0110111};
 
+  // std::cout << "Enter sensor (Temp IN, Temp OUT, Motion, Ligth): ";
+  // std::string input;
+  // std::stringstream sens;
+  // getline(std::cin, input);
+  // sens << input;
+  // sens >> sensTempOut >> sensTempIn >> sensMotion >> sensLigth;
 
-  //std::cout << "Enter sensor (Temp IN, Temp OUT, Motion, Ligth): ";
-  //std::string input;
-  //std::stringstream sens;
-  //getline(std::cin, input);
-  //sens << input;
-  //sens >> sensTempOut >> sensTempIn >> sensMotion >> sensLigth;
-
-	std::cout << statusText(status);
+  std::cout << statusText(statusOld, statusNew, 19);
 
   return 0;
 }

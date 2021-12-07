05ae52635df87c0d8ac8d18129402a709dab9176
PC <PC@>
2021-12-07 22:06:39 +0300

    задача 4

---
+++ module18/task4/task4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+void evendigits(long long n, int &ans) {
+  if (n == 0) return;
+  if ((n % 10) % 2 == 0) ++ans;
+  evendigits(n / 10, ans);
+}
+
+int main() {
+  int ans = 0;
+  long long n;
+  std::cout << "Enter n: ";
+  std::cin >> n;
+  evendigits(n, ans);
+  std::cout << "ans = " << ans;
+}

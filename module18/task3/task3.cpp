c794f7f9bae80b83cefc435adf73f3779c68d950
PC <PC@>
2021-12-07 22:05:43 +0300

    задача 3

---
+++ module18/task3/task3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+// rabbit +1 +2 +3 -> n
+
+int numMethod(int n, int k = 3) {
+  if (n == 0) return 1;
+  int step = 0;
+  for (int i = 1; i <= (n > k ? k : n); i++) {
+    step += numMethod(n - i, k);
+  }
+  return step;
+}
+
+int main() {
+  int n, k;
+  std::cout << "Enter n= ";
+  std::cin >> n;
+  std::cout << "Enter k= ";
+  std::cin >> k;
+  if (k > 0) {
+    std::cout << numMethod(n, k);
+  } else {
+    std::cout << numMethod(n);
+  }
+}

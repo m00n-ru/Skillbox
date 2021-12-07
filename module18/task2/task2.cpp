b7036a5a3cef3e6e4991cdfa62a8fd8f95605d82
PC <PC@>
2021-12-07 22:04:58 +0300

    задача 2

---
+++ module18/task2/task2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+// rabbit +1 +2 +3 -> n
+
+int numMethod(int n) {
+  if (n == 0) {
+    return 1;
+  } else if (n == 1) {
+    return numMethod(n - 1);
+  } else if (n == 2) {
+    return numMethod(n - 1) + numMethod(n - 2);
+  } else {
+    return numMethod(n - 1) + numMethod(n - 2) + numMethod(n - 3);
+  }
+}
+
+int main() {
+  int n;
+  std::cout << "Enter n= ";
+  std::cin >> n;
+  std::cout << numMethod(n);
+}

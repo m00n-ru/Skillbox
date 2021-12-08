626fbdc18fd70b6687168f51b1a68aaab5125165
PC <PC@>
2021-12-08 20:00:27 +0300

    fix

--- module18/task4/task4.cpp
+++ module18/task4/task4.cpp
@@ -1,13 +1,13 @@
 ﻿#include <iostream>
 
 void evendigits(long long n, int &ans) {
-  if (n == 0) return;
   if ((n % 10) % 2 == 0) ++ans;
+  if (n/10 == 0) return;
   evendigits(n / 10, ans);
 }
 
 int main() {
-  int ans = 0;
+  int ans = 0 ;
   long long n;
   std::cout << "Enter n: ";
   std::cin >> n;

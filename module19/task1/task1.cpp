83dd31ed07df515600f0e8ac6232c6a25a92f207
PC <PC@>
2021-12-14 20:17:14 +0300

    задача 1

---
+++ module19/task1/task1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main() {
+	std::string word;
+	std::ifstream file;
+	int count = 0;
+	file.open(".\\words.txt", std::ios::binary);
+
+	std::cout << "Enter word: ";
+	std::cin >> word;
+	while (!file.eof()) {
+		std::string fWord;
+		file >> fWord;
+		if (fWord == word) ++count;
+	}
+	std::cout << "Number of words " << word  << ": " << count;
+	file.close();
+}

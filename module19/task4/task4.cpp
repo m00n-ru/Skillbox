c9a6318aaddb23d7a1b1238747f57c8884d9db2c
PC <PC@>
2021-12-16 21:58:42 +0300

    задача 4

---
+++ module19/task4/task4.cpp
@@ -0,0 +1,31 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+  std::ifstream file;
+  std::string pathF;
+
+  do {
+    std::cout << "File path: ";
+    std::cin >> pathF;
+    file.open(pathF, std::ios::binary);
+    if (!file.is_open()) {
+      std::cout << "Error file path!" << std::endl;
+    }
+  } while (!file.is_open());
+
+  char strF[4];
+  file.read(strF, 4);
+
+  if (strF[0] == -119 && strF[1] == 'P' && strF[2] == 'N' && strF[3] == 'G') {
+    if (pathF.substr(pathF.size() - 4) != ".png") {
+      std::cout << "Incorrect file extension" << std::endl;
+    }
+    std::cout << "File has PNG format";
+  } else {
+    std::cout << "File of UNKNOWN format";
+  }
+  file.close();
+}

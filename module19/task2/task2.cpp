7dcc9043c28c54632ac536aae38ab313592ed7cb
PC <PC@>
2021-12-16 21:22:30 +0300

    задача 2

---
+++ module19/task2/task2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main() {
+	std::ifstream file;
+	std::string pathF;
+	char s[30];
+
+	do {
+		std::cout << "File path: ";
+		std::cin >> pathF;
+		file.open(pathF, std::ios::binary);
+		if (!file.is_open()) std::cout << "Error file path!" << std::endl;
+	} while (!file.is_open());
+
+	while (file.read(s, sizeof(s) - 1)) {
+		s[file.gcount()] = '\0';
+		std::cout << s;
+	}
+
+	file.close();
+}

b10a76933965c883ec5a259bde03bf854d3657b9
PC <PC@>
2021-12-18 18:53:35 +0300

    задача 2

---
+++ module20/task2/task2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+	int w, h;
+	std::ofstream file("pic.txt");
+	std::srand(std::time(nullptr));
+	std::cout << "Enter width and high: ";
+	std::cin >> w >> h;
+
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			int num = std::rand()%2;
+			file << num;
+		}
+		file << std::endl;
+	}
+}

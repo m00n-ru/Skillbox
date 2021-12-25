c456bb45849f95a152140c6047a54cb797cca017
PC <PC@>
2021-12-25 23:02:02 +0300

    задача 2

---
+++ module22/task2/task2.cpp
@@ -0,0 +1,40 @@
+﻿#include <map>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string input;
+	std::map<std::string, std::string> reg;
+
+	while (true) {
+		std::cout << "Enter: ";
+		std::cin >> input;
+
+		if (input == "Next") {
+			std::cout << "Next: " << reg.begin()->second << std::endl;
+			reg.erase(reg.begin());
+		} else if (input == "q") {
+			break;
+		} else {
+			std::pair<std::map<std::string, std::string>::iterator, bool> number;
+			number = reg.insert({input, input});
+			if (!number.second) {
+				int count = 1;
+				std::string repit = input;
+				while (number.first->first == repit) {
+					++number.first;
+					repit = input + std::to_string(count);
+					if (number.first == reg.end() || number.first->first != repit) {
+						reg[repit] = input;
+						break;
+					}
+					++count;
+				}
+			}
+		}
+	}
+	std::cout << std::endl;
+	for (std::pair<std::string, std::string> i : reg) {
+		std::cout << i.second << std::endl;
+	}
+}

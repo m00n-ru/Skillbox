182a7e0c4c54ad8c08d75e0462fbd63d0ffa3234
PC <PC@>
2021-12-26 20:23:43 +0300

    задача 2

--- module22/task2/task2.cpp
+++ module22/task2/task2.cpp
@@ -4,37 +4,34 @@
 
 int main() {
 	std::string input;
-	std::map<std::string, std::string> reg;
+	std::map<std::string, int> reg;
 
 	while (true) {
 		std::cout << "Enter: ";
 		std::cin >> input;
 
 		if (input == "Next") {
-			std::cout << "Next: " << reg.begin()->second << std::endl;
-			reg.erase(reg.begin());
+			if (reg.empty()) {
+				std::cout << "The queue is empty" << std::endl;
+				continue;
+			}
+			std::cout << "Next: " << reg.begin()->first << std::endl;
+			if (reg.begin()->second == 1) {
+				reg.erase(reg.begin());
+			} else {
+				--reg.begin()->second;
+			}
 		} else if (input == "q") {
 			break;
 		} else {
-			std::pair<std::map<std::string, std::string>::iterator, bool> number;
-			number = reg.insert({input, input});
-			if (!number.second) {
-				int count = 1;
-				std::string repit = input;
-				while (true) {
-					++number.first;
-					repit = input + std::to_string(count);
-					if (number.first == reg.end() || number.first->first != repit) {
-						reg[repit] = input;
-						break;
-					}
-					++count;
-				}
+			std::pair<std::map<std::string, int>::iterator, bool> pair_input = reg.insert({input, 1});
+			if (!pair_input.second) {
+				++pair_input.first->second;
 			}
 		}
 	}
 	std::cout << std::endl;
-	for (std::pair<std::string, std::string> i : reg) {
-		std::cout << i.second << std::endl;
+	for (const std::pair<std::string, int> i : reg) {
+		std::cout << i.first << " " << i.second << std::endl;
 	}
 }

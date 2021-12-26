10994aafa91a1f9fcefb7a71bf8bff81f45336ab
PC <PC@>
2021-12-26 20:49:10 +0300

    задача 3

--- module22/task3/task3.cpp
+++ module22/task3/task3.cpp
@@ -1,45 +1,22 @@
 ﻿#include <map>
 #include <iostream>
 
+std::map<char, int> count_char(std::string& st) {
+	std::map<char, int> count;
+	for (char& i : st) {
+		std::pair<std::map<char, int>::iterator, bool> pair_input;
+		pair_input = count.insert({i, 1});
+		if (!pair_input.second) {
+			++pair_input.first->second;
+		}
+	}
+	return count;
+}
+
 bool anagram(std::string& st1, std::string& st2) {
-	std::map<int, bool> str;
-	std::pair<std::map<int, bool>::iterator, bool> pair_chr;
-
-	for (char& i : st1) {
-		pair_chr = str.insert({(int)i * 100, true});
-		if (!pair_chr.second) {
-			int count = 1;
-			int repit = (int)i;
-			while (true) {
-				++pair_chr.first;
-				repit = (int)i * 100 + count;
-				if (pair_chr.first == str.end() || pair_chr.first->first != repit) {
-					str[repit] = true;
-					break;
-				}
-				++count;
-			}
-		}
-
-	}
-
-	for (char& i : st2) {
-		int int_i = (int)i * 100;
-
-		while (true) {
-			if (str.find(int_i) != str.end()) {
-				if (str.find(int_i)->second) {
-					str.find(int_i)->second = false;
-					break;
-				} else {
-					++int_i;
-				}
-			} else {
-				return false;
-			}
-			return true;
-		}
-	}
+	std::map<char, int> count_char_str1 = count_char(st1);
+	std::map<char, int> count_char_str2 = count_char(st2);
+	return count_char_str1 == count_char_str2;
 }
 
 int main() {
@@ -51,9 +28,5 @@ int main() {
 	std::cout << "Enter string 2:";
 	std::cin >> input2;
 
-	if (anagram(input1, input2)) {
-		std::cout << "Yes";
-	} else {
-		std::cout << "No";
-	}
+	std::cout << (anagram(input1, input2) ? "Yes" : "No");
 }

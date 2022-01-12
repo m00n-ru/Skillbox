847ad7c715d934f5c7d080ba8f7c099d0b30b1a9
PC <PC@>
2022-01-12 18:31:40 +0300

    task3

---
+++ module23/task3/task3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+#define SPRING 1
+#define SUMMER 2
+#define AUTUMN 3
+#define WINTER 4
+
+#define TY SUMMER
+
+int main() {
+#if TY == SPRING
+	std::cout << "Spring";
+#elif TY == SUMMER
+	std::cout << "Summer";
+#elif TY == AUTUMN
+	std::cout << "Autumn";
+#elif TY == WINTER
+	std::cout << "Winter";
+#endif	
+}

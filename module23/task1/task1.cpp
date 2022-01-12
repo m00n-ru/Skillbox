78ea73b38a416d20bc997fba960282aee18ec8dc
PC <PC@>
2022-01-12 18:31:27 +0300

    task1

---
+++ module23/task1/task1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+#define DAY(d) (((d) == 1) ? "Mo" : \
+				((d) == 2) ? "Tu" : \
+				((d) == 3) ? "We" : \
+				((d) == 4) ? "Th" : \
+			    ((d) == 5) ? "Fr" : \
+			    ((d) == 6) ? "Su" : \
+			    ((d) == 7) ? "Sa" : "Input error")	
+
+int main() {
+	int d;
+	std::cin >> d;
+	std::cout << DAY(d);
+}

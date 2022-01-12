fbb4f24d41c4a88a239385291b79c451a5772b16
PC <PC@>
2022-01-12 18:31:34 +0300

    task2

---
+++ module23/task2/task2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+#define FOR_FUNC(func,arr) for(int i = 0; i < 10; i++){func(arr, i);}
+
+#define COUNT(arr, sum) for(int i : arr) { sum+= i;}\
+						std::cout << "All passengers: " <<sum; 
+
+void input(int* a, int i) {
+	std::cout << "Input passengers in " << i << " wagon: ";
+	std::cin >> a[i];
+}
+
+void overflow(int* a, int i) {
+	if( a[i] > 20 ) {
+		std::cout << i + 1 << " wagon overflow" << std::endl;
+	}
+}
+
+void free(int* a, int i) {
+	if( a[i] < 20 ) {
+		std::cout << i + 1 << " wagon free" << std::endl;
+	}
+}
+
+void count(int* a, int i) {
+	if( a[i] < 20 ) {
+		std::cout << i + 1 << " wagon free" << std::endl;
+	}
+}
+
+
+int main() {
+	int wagon[10];
+	int sum = 0;
+	FOR_FUNC(input, wagon)
+	FOR_FUNC(overflow, wagon)
+	FOR_FUNC(free, wagon)
+	COUNT(wagon, sum)
+}

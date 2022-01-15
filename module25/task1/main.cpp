c1ae12c02aaacb0a1ad7e3bbff92e42db2a27578
PC <PC@>
2022-01-15 13:45:00 +0300

    task1

---
+++ module25/task1/main.cpp
@@ -0,0 +1,26 @@
+﻿#include "func.h"
+#include "struct.h"
+
+
+int main() {
+  Coord coord {};
+
+  std::cout << "Operation START \t" << std::endl;
+  scalpel(coord);
+  while ( true ) {
+	std::string com;
+	std::cout << "Enter command: ";
+	std::cin >> com;
+	if ( com == "hemostat" ) {
+	  hemostat(coord);
+	} else if ( com == "tweezers" ) {
+	  tweezers(coord);
+	} else if ( com == "suture" ) {
+	  if ( suture(coord) );
+	  break;
+	} else {
+	  std::cout << "Unknown command" << std::endl;
+	}
+  }
+  std::cout << "Operation END" << std::endl;
+}

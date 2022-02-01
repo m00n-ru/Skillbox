1800e28d0950cb5ae588ee72980e65a33e3292e9
m00n_ru <none>
2022-02-01 11:21:12 +0300

    task 2

---
+++ module30/task2/task2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cpr/cpr.h>
+
+
+int main() {
+  cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/html"));
+
+  std::string header = r.text;
+  int begin = 0;
+  int end = 0;
+
+  if (header.find("<h1>") != std::string::npos) {
+    begin = header.find("<h1>") + 4;
+  }
+
+  if (header.find("</h1>") != std::string::npos) {
+    end = header.find("</h1>") - begin;
+  }
+
+  std::cout << header.substr(begin, end) << std::endl;
+}
+
+
